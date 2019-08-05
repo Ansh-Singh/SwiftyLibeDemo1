78d9196c8801a0390a61f4fc8cdd8941cf4d3aca
milan-hipl <51443694+milan-hipl@users.noreply.github.com>
2019-08-05 14:53:00 +0530

    Added test cases

---
+++ SwiftyLibDemo1/SwiftyLib1.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib1.swift
+//  SwiftyLibDemo1
+//
+//  Created by Milan Katiyar on 05/08/19.
+//  Copyright © 2019 Milan Katiyar. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib1 {
+    
+    let name = "SwiftyLib1"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SwiftyLibDemo1Tests/SwiftyLibDemo1Tests.swift
+++ SwiftyLibDemo1Tests/SwiftyLibDemo1Tests.swift
@@ -10,15 +10,27 @@ import XCTest
 @testable import SwiftyLibDemo1
 
 class SwiftyLibDemo1Tests: XCTestCase {
+    
+     var swiftyLib1: SwiftyLib1!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
+         swiftyLib1 = SwiftyLib1()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testAdd() {
+        XCTAssertEqual(swiftyLib1.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(swiftyLib1.sub(a: 2, b: 1), 1)
+    }
+
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
