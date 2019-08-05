97c5cc3e3aa60ae17b690ba45bae758c550f8f19
milan-hipl <51443694+milan-hipl@users.noreply.github.com>
2019-08-05 13:57:02 +0530

    Initial project setup

---
+++ SwiftyLibDemo1/SwiftyLibDemo1.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLibDemo1.h
+//  SwiftyLibDemo1
+//
+//  Created by Milan Katiyar on 05/08/19.
+//  Copyright © 2019 Milan Katiyar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyLibDemo1.
+FOUNDATION_EXPORT double SwiftyLibDemo1VersionNumber;
+
+//! Project version string for SwiftyLibDemo1.
+FOUNDATION_EXPORT const unsigned char SwiftyLibDemo1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibDemo1/PublicHeader.h>
+
+
---
+++ SwiftyLibExamples1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples1
+//
+//  Created by Milan Katiyar on 05/08/19.
+//  Copyright © 2019 Milan Katiyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
